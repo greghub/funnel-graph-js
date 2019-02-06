d5c097e6110265fe3fd0d70d8ae39faceabe4a79
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2019-02-06 15:16:47 +0400

    Setup tests

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const assert = require('assert');
+
+describe('Basic Test', () => {
+    it('basic test', () => {
+        assert.equal('Hello'.length, 5);
+    });
+});
