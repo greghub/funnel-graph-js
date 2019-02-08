b7bb65dba0c8709c79e06cb5b33ff65f591fd27d
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2019-02-08 15:39:38 +0400

    Add tests for path functions

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { roundPoint, formatNumber } from '../src/js/number';
+import { createCurves, createVerticalCurves } from '../src/js/path';
 
 const assert = require('assert');
 
@@ -10,5 +11,15 @@ describe('Test number functions', () => {
 
     it('number format test', () => {
         assert.equal(formatNumber(12500), '12,500');
+    });
+});
+
+describe('Add tests for paths', () => {
+    it('can create points for curves', () => {
+        assert.equal(createCurves(0, 0, 6, 2), ' C3,0 3,2 6,2');
+    });
+
+    it('can create points for vertical curves', () => {
+        assert.equal(createVerticalCurves(0, 0, 6, 2), ' C0,1 6,1 6,2');
     });
 });
