cfc56a7501c260b3554fd4d25b48ea61bf8c9fcc
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2019-02-08 15:48:30 +0400

    Add tests for background generator

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import { roundPoint, formatNumber } from '../src/js/number';
 import { createCurves, createVerticalCurves } from '../src/js/path';
+import { generateLegendBackground } from '../src/js/graph';
 
 const assert = require('assert');
 
@@ -21,5 +22,29 @@ describe('Add tests for paths', () => {
 
     it('can create points for vertical curves', () => {
         assert.equal(createVerticalCurves(0, 0, 6, 2), ' C0,1 6,1 6,2');
+    });
+});
+
+describe('Add tests for background color generator', () => {
+    it('can generate a solid background', () => {
+        assert.equal(generateLegendBackground('red'), 'background-color: red');
+    });
+
+    it('can generate a solid background from an array with single element', () => {
+        assert.equal(generateLegendBackground(['red']), 'background-color: red');
+    });
+
+    it('can generate a gradient background', () => {
+        assert.equal(
+            generateLegendBackground(['red', 'orange']),
+            'background-image: linear-gradient(to right, red, orange)'
+        );
+    });
+
+    it('can generate a vertical gradient background', () => {
+        assert.equal(
+            generateLegendBackground(['red', 'orange'], 'vertical'),
+            'background-image: linear-gradient(red, orange)'
+        );
     });
 });
