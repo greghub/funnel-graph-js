8e125842349cd65d1ffa9e7d946be1c6eac212af
Greg Hovanesyan <greg.hovanesyan@gmail.com>
2019-02-08 15:35:32 +0400

    Add tests for number functions

--- test/test.js
+++ test/test.js
@@ -1,8 +1,14 @@
 /* eslint-disable no-undef */
+import { roundPoint, formatNumber } from '../src/js/number';
+
 const assert = require('assert');
 
-describe('Basic Test', () => {
-    it('basic test', () => {
-        assert.equal('Hello'.length, 5);
+describe('Test number functions', () => {
+    it('round number test', () => {
+        assert.equal(roundPoint(19.99999999998), 20);
+    });
+
+    it('number format test', () => {
+        assert.equal(formatNumber(12500), '12,500');
     });
 });
